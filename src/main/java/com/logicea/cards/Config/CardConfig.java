c914482572d397910232cd2c9f65a9f5b5d035d6
ekcareer <ekcareer@gmail.com>
2024-04-10 10:00:13 +0300

    add(seeder):seed more cards entities

--- src/main/java/com/logicea/cards/Config/CardConfig.java
+++ src/main/java/com/logicea/cards/Config/CardConfig.java
@@ -21,7 +21,24 @@ public class CardConfig {
             Card card3 = new Card(null, "card3", null, "description", CardStatus.TODO, null, null, 1, 1);
             Card card4 = new Card(null, "card4", null, "description", CardStatus.TODO, null, null, 1, 1);
 
-            repository.saveAll(List.of(card1, card2, card3, card4));
+            Card card5 = new Card(null, "card5", "#FF9933", "description", CardStatus.TODO, null, null, 1, 1);
+            Card card6 = new Card(null, "card6", null, "description", CardStatus.TODO, null, null, 2, 2);
+            Card card7 = new Card(null, "card7", null, "description", CardStatus.TODO, null, null, 1, 1);
+            Card card8 = new Card(null, "card8", null, "description", CardStatus.TODO, null, null, 1, 1);
+
+            Card card9 = new Card(null, "card9", "#FF9933", "description", CardStatus.TODO, null, null, 1, 1);
+            Card card10 = new Card(null, "card10", null, "description", CardStatus.TODO, null, null, 2, 2);
+            Card card11 = new Card(null, "card11", null, "description", CardStatus.TODO, null, null, 1, 1);
+            Card card12 = new Card(null, "card12", null, "description", CardStatus.TODO, null, null, 1, 1);
+
+            Card card13 = new Card(null, "card13", "#FF9933", "description", CardStatus.TODO, null, null, 1, 1);
+            Card card14 = new Card(null, "card14", null, "description", CardStatus.TODO, null, null, 2, 2);
+            Card card15 = new Card(null, "card15", null, "description", CardStatus.TODO, null, null, 1, 1);
+            Card card16 = new Card(null, "card16", null, "description", CardStatus.TODO, null, null, 1, 1);
+
+            repository.saveAll(
+                    List.of(card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12,
+                            card13, card14, card15, card16));
         };
     }
 
